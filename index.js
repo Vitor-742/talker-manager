89c9e804689f3ef6f5615208ed498b6a32f3c215
Vitor Campos <vitorcampos742@gmail.com>
2022-04-25 16:59:48 -0300

    requisitos 1 e 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,29 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', (req, res) => {
+  try {
+    const data = fs.readFileSync('./talker.json', 'utf8');
+    const parseData = JSON.parse(data);
+    res.status(200).json(parseData);
+  } catch (error) {
+    res.status(200).json([]);
+  }
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = fs.readFileSync('./talker.json', 'utf8');
+    const parseData = JSON.parse(data);
+    const peopleId = parseData.find((people) => people.id === parseInt(id, 10));
+    if (!peopleId) throw new Error();
+    res.status(200).json(peopleId);
+  } catch (err) {
+    res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
 });
